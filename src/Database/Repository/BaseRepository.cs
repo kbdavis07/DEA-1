e4ed3c78f6927868aba283062e46de840a05171c
Brian Davis <kbdavis07@yahoo.com>
2017-04-05 07:42:50 -0400

    Taking using statement out to prevent connection from closing too soon.

--- src/Database/Repository/BaseRepository.cs
+++ src/Database/Repository/BaseRepository.cs
@@ -1,4 +1,4 @@
-﻿using System.Data.Entity;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -6,43 +6,39 @@ namespace DEA.Database.Repository
 {
     public static class BaseRepository<TEntity> where TEntity : class
     {
+        //Must have this here otherwise the context closes too soon and won't save or get from database!
+        private static readonly DEAContext db = new DEAContext();
 
         public static async Task InsertAsync(TEntity entity)
         {
-            using (var db = new DEAContext())
-            {
+           
                 db.Set<TEntity>().Add(entity);
                 db.Entry(entity).State = EntityState.Added;
                 await db.SaveChangesAsync();
-            }
+            
         }
 
         public static async Task UpdateAsync(TEntity entity)
         {
-            using (var db = new DEAContext())
-            {
+            
                 db.Set<TEntity>().Add(entity);
                 db.Entry(entity).State = EntityState.Modified;
                 await db.SaveChangesAsync();
-            }
+            
         }
 
         public static async Task DeleteAsync(TEntity entity)
         {
-            using (var db = new DEAContext())
-            {
+            
                 db.Set<TEntity>().Remove(entity);
                 db.Entry(entity).State = EntityState.Deleted;
                 await db.SaveChangesAsync();
-            } 
+            
         }
 
         public static IQueryable<TEntity> GetAll()
         {
-            using (var db = new DEAContext())
-            {
-                return db.Set<TEntity>();
-            }   
+           return db.Set<TEntity>();
         }
 
     }
